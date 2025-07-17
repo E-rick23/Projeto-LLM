e0b591219eebc1e4e7c7dfb8596e9b5c1c3a2720
e-rick23 <erickmarques63@gmail.com>
2025-07-17 02:55:23 -0300

    Corrigido bug que fazia a exclamação se manter após a inserção do nome.

--- js/saudacao.js
+++ js/saudacao.js
@@ -3,18 +3,24 @@ let saudacaoBase = ""; // Variável para armazenar a saudação inicial
 function mostrarSaudacao() {
     const horas = obterHoraAtual(); // Usa a nova função para obter a hora
     if (horas < 12 && horas > 5) {
-        saudacaoBase = "Bom dia!";
+        saudacaoBase = "Bom dia";
     } else if (horas < 18 && horas > 12) {
-        saudacaoBase = "Boa tarde!";
+        saudacaoBase = "Boa tarde";
     } else {
-        saudacaoBase = "Boa noite!";
+        saudacaoBase = "Boa noite";
     }
     atualizarSaudacao(); // Atualiza o texto exibido
 }
 
 function atualizarSaudacao(nome = "") {
     const saudacaoElement = document.getElementById("saudacao");
-    saudacaoElement.textContent = nome ? `${saudacaoBase}, ${nome}!` : saudacaoBase;
+    
+    // Adiciona pontuação diferente dependendo da presença do nome
+    if (nome) {
+        saudacaoElement.textContent = `${saudacaoBase}, ${nome}!`;
+    } else {
+        saudacaoElement.textContent = `${saudacaoBase}!`;
+    }
 }
 
 function obterHoraAtual() {
